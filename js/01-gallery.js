1473cc12c5613accf3c8af2389cf3b226c6d440f
Andy Fartushnyi <andyfartushniy@gmail.com>
2023-04-21 00:11:32 +0300

    Updated code for task 7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryEl.addEventListener('click', onImageClick);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+        </li>`
+        })
+        .join("");
+};
+
+function onImageClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+
+    // return evt.target.dataset.source;
+}
+console.log(onImageClick(evt));
